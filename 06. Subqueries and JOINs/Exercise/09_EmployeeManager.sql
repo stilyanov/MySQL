e702b0e86fe1a20508620d84ad1b53c8524787f2
stilyanov <122092690+stilyanov@users.noreply.github.com>
2024-01-29 21:36:57 +0200

    added second way to solve the problem

--- 06. Subqueries and JOINs/Exercise/09_EmployeeManager.sql
+++ 06. Subqueries and JOINs/Exercise/09_EmployeeManager.sql
@@ -1,3 +1,4 @@
+--#1
 SELECT e2.employee_id,
 	e2.first_name,
 	e2.manager_id,
@@ -7,3 +8,12 @@ SELECT e2.employee_id,
 FROM employees e2
 WHERE manager_id IN (3, 7)
 ORDER BY first_name;
+--#2
+SELECT e.employee_id, 
+	e.first_name, 
+	e.manager_id, 
+	m.first_name AS manager_id
+FROM employees e
+	JOIN employees m ON e.manager_id = m.employee_id
+WHERE e.manager_id IN (3,7)
+ORDER BY e.first_name;
