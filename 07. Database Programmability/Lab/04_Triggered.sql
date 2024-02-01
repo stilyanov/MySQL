1dec0b91bcdf2a1e5e1935c8464af3683fa1d3e3
stilyanov <122092690+stilyanov@users.noreply.github.com>
2024-02-01 16:15:34 +0200

    Update 04_Triggered.sql

--- 07. Database Programmability/Lab/04_Triggered.sql
+++ 07. Database Programmability/Lab/04_Triggered.sql
@@ -13,4 +13,4 @@ AFTER DELETE
 ON employees
 FOR EACH ROW
     INSERT INTO deleted_employees(first_name, last_name, middle_name, job_title, department_id, salary)
-    VALUE (OLD.first_name, OLD.last_name, OLD.middle_name, OLD.job_title, OLD. department_id, OLD.salary);
+    VALUE (OLD.first_name, OLD.last_name, OLD.middle_name, OLD.job_title, OLD.department_id, OLD.salary);
