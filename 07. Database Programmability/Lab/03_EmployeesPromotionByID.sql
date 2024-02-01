67796c1cf2523cffc76b566750e273e7b59f53e1
stilyanov <v.stilyanov@gmail.com>
2024-02-01 13:03:30 +0200

    Database Programmability lab

---
+++ 07. Database Programmability/Lab/03_EmployeesPromotionByID.sql
@@ -0,0 +1,18 @@
+DELIMITER $$
+
+CREATE PROCEDURE usp_raise_salary_by_id(id INT)
+BEGIN
+    START TRANSACTION;
+    IF ((SELECT COUNT(employee_id) FROM employees WHERE employee_id LIKE id) <> 1)
+    THEN
+        ROLLBACK;
+    ELSE
+        UPDATE employees a
+        SET salary = salary * 1.05
+        WHERE employee_id = id;
+    END IF;
+END $$;
+
+DELIMITER ;
+
+CALL usp_raise_salary_by_id(17);
---
+++ 07. Database Programmability/Lab/04_Triggered.sql
@@ -0,0 +1,16 @@
+CREATE TABLE deleted_employees(
+    employee_id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(20),
+    last_name VARCHAR(20),
+    middle_name VARCHAR(20),
+    job_title VARCHAR(50),
+    department_id INT,
+    salary DOUBLE
+);
+
+CREATE TRIGGER tr_deleted_employees
+AFTER DELETE
+ON employees
+FOR EACH ROW
+    INSERT INTO deleted_employees(first_name, last_name, middle_name, job_title, department_id, salary)
+    VALUE (OLD.first_name, OLD.last_name, OLD.middle_name, OLD.job_title, OLD. department_id, OLD.salary);
